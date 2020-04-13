c7bf1878b20fa7fac5f25da27aaf8daccc4b7832
codecamp-official-thailand <codecamp.official.thailand@gmail.com>
2020-04-13 17:53:24 +0700

    Completed Full Stack

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -11,7 +11,7 @@ import {
   notification,
 } from "antd";
 import "./App.css";
-import axios from "axios";
+import axios from "../config/axios";
 
 const { Text } = Typography;
 const { confirm } = Modal;
@@ -21,7 +21,7 @@ function App() {
   const [inputValue, setInputValue] = useState("");
 
   const fetchData = async () => {
-    const result = await axios.get("http://localhost:8000/tasks");
+    const result = await axios.get("/tasks");
     setTodoList(result.data);
   };
 
@@ -37,7 +37,7 @@ function App() {
     const body = {
       isCompleted: e.target.checked,
     };
-    await axios.put(`http://localhost:8000/tasks/${e.target.id}`, body);
+    await axios.put(`/tasks/${e.target.id}`, body);
     fetchData();
   };
 
@@ -47,7 +47,7 @@ function App() {
       // icon: <ExclamationCircleOutlined />,
       content: `Do you want to delete Task ID: ${id}`,
       onOk() {
-        axios.delete(`http://localhost:8000/tasks/${id}`).then((result) => {
+        axios.delete(`/tasks/${id}`).then((result) => {
           notification.success({
             message: `Delete Task ID: ${id}`,
             description: `You have been deleted task ID: ${id} permanently`,
@@ -65,7 +65,7 @@ function App() {
     const body = {
       task: inputValue,
     };
-    await axios.post("http://localhost:8000/tasks", body);
+    await axios.post("/tasks", body);
     fetchData();
     setInputValue("");
   };
